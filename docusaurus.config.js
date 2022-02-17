55cee32f96d3df676d40832df20bbf4c51a83018
Robert Guss <robertguss@pm.me>
2022-02-17 09:52:58 -0500

    updated favicon and log

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
-  favicon: "img/favicon.ico",
+  favicon: "img/favicon.png",
   organizationName: "cypress-io", // Usually your GitHub org/user name.
   projectName: "github-action", // Usually your repo name.
 
